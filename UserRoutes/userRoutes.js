6805584acf6e75447f31593f210f9a81d11a1899
sideeq12 <abdwaheed2018@gmail.com>
2021-12-21 23:03:19 +0000

    layer models

--- UserRoutes/userRoutes.js
+++ UserRoutes/userRoutes.js
@@ -1,15 +1,19 @@
 const express = require("express");
+const req = require("express/lib/request");
 const router = express.Router();
 // import {registerUser} from "../controller/userController"
-const User = require("../models/userModel")
+const myUsers = require("../models/userModel")
 
 router.post("/signup", (req, res) =>{
-    const signedUpUser = new User({
+    console.log(req.body)
+    console.log(myUsers)
+    const signedUpUser = new myUsers({
         full_name : req.body.full_name,
         email : req.body.email,
         password : req.body.password,
         tags : req.body.tags 
     })
+    console.log(req.body)
     signedUpUser.save()
     .then(data =>{
         res.json(data)
--- models/userModel.js
+++ models/userModel.js
@@ -1,42 +1,48 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs")
+const Schema = mongoose.Schema
 
-const userSchema = mongoose.Schema({
-    full_name : {
-        type : String,
-        required : true,
-    }, 
-    email : {
-        type : String,
-        required : true,
-        unique : true
-    },
-    password : {
-        type : String,
-        required : true,
-    }, 
-    tags : {
-        type : String,
-        required : true,
-    },
-    description : {
-        type : String,
-        required : true
-    },
-    date : {
-        type : Date,
-        default : Date.now
-    }
-}, {
-    timestamps : true
+const userSchema = new Schema({
+    full_name : String,
+    email : String,
+    tags : String,
+    password : String
 })
- userSchema.pre("save", async function(next){
-     if(!this.isModified("password")){
-         next()
-     }
-     const salt = bcrypt.genSalt(10)
-     this.password = await bcrypt.hash(this.password, salt)
- } )
-const User = mongoose.model("Users", userSchema)
+//     full_name : {
+//         type : String,
+//         required : true,
+//     }, 
+//     email : {
+//         type : String,
+//         required : true,
+//         unique : true
+//     },
+//     password : {
+//         type : String,
+//         required : true,
+//     }, 
+//     tags : {
+//         type : String,
+//         required : true,
+//     },
+//     description : {
+//         type : String,
+//         required : true
+//     },
+//     date : {
+//         type : Date,
+//         default : Date.now
+//     }
+// }, {
+//     timestamps : true
+// })
+//  userSchema.pre("save", async function(next){
+//      if(!this.isModified("password")){
+//          next()
+//      }
+//      const salt = bcrypt.genSalt(10)
+//      this.password = await bcrypt.hash(this.password, salt)
+//  } )
+const myUsers = mongoose.model("Users", userSchema)
 
-module.exports = {User}
+module.exports = { myUsers }
