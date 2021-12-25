0f5b0b2accb8f5d6807f65d211fd5bdcb6b32b0a
sideeq12 <abdwaheed2018@gmail.com>
2021-12-25 15:10:41 +0000

    api testing

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ mongoose.connect(process.env.DATABASE_ACCESS, ()=>{
 app.use(express.json())
 app.use(cors())
 app.use("/api", routesUrl)
+app.get("/", (req, res)=>{ res.json({status : "Success API"})})
 
 
 app.listen(8080, ()=>{
